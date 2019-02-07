f68647a85a6b60f2dd4551095d3370abcd4714bf
chibuikekenneth <chibuikekenneth003@gmail.com>
2019-02-07 18:13:56 +0100

    handlers for new and loggedin users

---
+++ controllers/authControllers.go
@@ -0,0 +1,35 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/chibuikekenneth/go-transactions/models"
+	u "github.com/chibuikekenneth/go-transactions/utils"
+)
+
+var CreateAccount = func(w http.ResponseWriter, r *http.Request) {
+
+	account := &models.Account{}
+	err := json.NewDecoder(r.Body).Decode(account) //decode the request body into struct and failed if any error occur
+	if err != nil {
+		u.Respond(w, u.Message(false, "Invalid request"))
+		return
+	}
+
+	resp := account.Create() //Create account
+	u.Respond(w, resp)
+}
+
+var Authenticate = func(w http.ResponseWriter, r *http.Request) {
+
+	account := &models.Account{}
+	err := json.NewDecoder(r.Body).Decode(account) //decode the request body into struct and failed if any error occur
+	if err != nil {
+		u.Respond(w, u.Message(false, "Invalid request"))
+		return
+	}
+
+	resp := models.Login(account.Email, account.Password)
+	u.Respond(w, resp)
+}
